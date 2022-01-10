07658cddea86910674aa170ec855cb363ba156ed
DarkoVasic98 <darevasic010@gmail.com>
2022-01-10 20:12:33 +0100

    Dodata putanja za logout

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
 
     Route::apiResource('parkings', ParkingController::class);
 });
